1990f2d76821f734b426d83bb097d0b41eec2408
warnerfleming <fleming14@usf.edu>
2024-12-05 20:20:04 -0500

    start

---
+++ public/Bubbly.js/bubbly.js
@@ -0,0 +1,14 @@
+//Was very hard to do with touchscreen keyboard
+
+const BubbleChart = ({ data }) => {
+    const Data = {
+      labels: data.months,
+      datasets: [
+        {
+          label: ',
+          data: data.sales,
+          backgroundColor:
+          borderColor: ,
+          borderWidth: ,
+        },
+      ],
